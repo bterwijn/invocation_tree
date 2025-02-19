5e7f35ec7764bfa680b9330cf0a88ade21b30ce4
Bas Terwijn <bterwijn@gmail.com>
2025-02-20 00:33:27 +0100

    highlighting changes

--- invocation_tree/__init__.py
+++ invocation_tree/__init__.py
@@ -16,14 +16,15 @@ def highlight_diff(str1, str2):
     result = []
     for tag, i1, i2, j1, j2 in matcher.get_opcodes():
         if tag == 'replace':
-            result.append(f"<B>{str1[i1:i2]}</B>")
+            result.append(f'<B>{str2[j1:j2]}</B>&#8203;')
         elif tag == 'delete':
-            result.append(f"--")
+            result.append(f'<FONT COLOR="#aaaaaa"><B>-</B></FONT>&#8203;')
         elif tag == 'insert':
-            result.append(f"<B>{str2[j1:j2]}</B>")
+            result.append(f'<B>{str2[j1:j2]}</B>&#8203;')
         elif tag == 'equal':
-            result.append(str1[i1:i2])
-    return ''.join(result)
+            result.append(str2[j1:j2])
+    diff = ''.join(result)
+    return diff
 
 class Tree_Node:
 
@@ -38,10 +39,11 @@ class Tree_Node:
 
 class Invocation_Tree:
 
-    def __init__(self, output_filename='invocation_tree.pdf', block=True, src_location=True, max_string_len=100, indent='   ',
+    def __init__(self, output_filename='invocation_tree.pdf', show=True, block=True, src_location=True, max_string_len=100, indent='   ',
                  color_paused = '#ccffcc', color_active = '#ffffff', color_returned = '#ffcccc', each_line=False, to_string=None):
         # --- config
         self.output_filename = output_filename
+        self.show = show
         self.output_count = -1
         self.block = block
         self.src_location = src_location
@@ -64,11 +66,11 @@ class Invocation_Tree:
         for tree_node in self.stack:
             print(tree_node)
 
-    def value_to_string(self, value, name):
+    def value_to_string(self, key, value):
         if id(value) in self.to_string:
             return self.to_string[id(value)](value)
-        elif name in self.to_string:
-            return self.to_string[name](value)
+        elif key in self.to_string:
+            return self.to_string[key](value)
         elif type(value) in self.to_string:
             return self.to_string[type(value)](value)
         else:
@@ -76,6 +78,16 @@ class Invocation_Tree:
             if len(val_str) > self.max_string_len:
                 val_str = '...'+val_str[-self.max_string_len:]    
             return html.escape(val_str)
+
+    def get_hightlighted_content(self, tree_node, key, value):
+        content = self.value_to_string(key, value)
+        if key in tree_node.strings:
+            old_content = tree_node.strings[key]
+            hightlighted_content = highlight_diff(old_content, content)
+        else:
+            hightlighted_content = '<B>'+content+'</B>'
+        tree_node.strings[key] = content
+        return hightlighted_content
 
     def build_html_table(self, tree_node, active=False, returned=False):
         function_name = tree_node.frame.f_code.co_name
@@ -89,21 +101,19 @@ class Invocation_Tree:
         if returned:
             color = self.color_returned
         table = f'<\n<TABLE BORDER="{str(border)}" CELLBORDER="0" CELLSPACING="1" BGCOLOR="{color}">\n  <TR>'
-        content = self.value_to_string(function_name, 'function_'+function_name)
-        table += '<TD ALIGN="left">'+ content +'</TD>'
-        tree_node.strings['__function_name__'] = content
+        hightlighted_content = self.get_hightlighted_content(tree_node, '__function_name__', function_name)
+        table += '<TD ALIGN="left">'+ hightlighted_content +'</TD>'
         for var,val in local_vars.items():
             if not '__' in var:
                 table += '</TR>\n  <TR>'
-                content = self.indent + self.value_to_string(var, function_name+'_'+var) + ': ' + \
-                                        self.value_to_string(val, function_name+'.'+var)
-                table += '<TD ALIGN="left">'+ content  +'</TD>'
-                tree_node.strings[var] = content
+                hightlighted_var = self.get_hightlighted_content(tree_node, function_name+'_'+var, var)
+                hightlighted_val = self.get_hightlighted_content(tree_node, function_name+'.'+var, val)
+                hightlighted_content = self.indent + hightlighted_var + ': ' + hightlighted_val
+                table += '<TD ALIGN="left">'+ hightlighted_content  +'</TD>'
         if returned:
             table += '</TR>\n  <TR>'
-            content = self.value_to_string(return_value, function_name+'.'+'return')
-            table += '<TD ALIGN="left">'+ 'return ' + content +'</TD>'
-            tree_node.strings['return'] = content
+            hightlighted_content = self.get_hightlighted_content(tree_node, 'return', return_value)
+            table += '<TD ALIGN="left">'+ 'return ' + hightlighted_content +'</TD>'
         table += '</TR>\n</TABLE>>'
         return table
             
@@ -123,11 +133,11 @@ class Invocation_Tree:
                 return '.'.join(splits)
         return self.output_filename
         
-    def render_graph(self, frame, event):
+    def output_graph(self, frame, event):
         graphviz_graph_attr = {}
         graphviz_node_attr = {'shape':'plaintext'}
         graphviz_edge_attr = {}
-        graph = Digraph(comment="recursion_graph",
+        graph = Digraph('invocation_tree',
                 graph_attr=graphviz_graph_attr,
                 node_attr=graphviz_node_attr,
                 edge_attr=graphviz_edge_attr)
@@ -135,7 +145,10 @@ class Invocation_Tree:
             graph.node(nid, label=table)
         for nid1, nid2 in self.edges:
             graph.edge(nid1, nid2)
-        graph.render(outfile=self.get_output_filename(), cleanup=True)
+        if self.show:
+            graph.view()
+        else:
+            graph.render(outfile=self.get_output_filename(), cleanup=False)
         if self.block:
             if self.src_location:
                 filename = frame.f_code.co_filename
@@ -153,7 +166,7 @@ class Invocation_Tree:
                 self.update_node(self.stack[-2])
                 self.add_edge(self.stack[-2], self.stack[-1])
             self.update_node(self.stack[-1], active=True)
-            self.render_graph(frame, event)
+            self.output_graph(frame, event)
         elif event == 'return':
             self.stack[-1].return_value = arg
             #self.print_stack()
@@ -161,10 +174,10 @@ class Invocation_Tree:
             if len(self.stack)>1:
                 self.update_node(self.stack[-2], active=True)
             self.stack.pop()
-            self.render_graph(frame, event)
+            self.output_graph(frame, event)
         elif event == 'line' and self.each_line:
             self.update_node(self.stack[-1], active=True)
-            self.render_graph(frame, event)
+            self.output_graph(frame, event)
         return self.trace_calls
     
     def __call__(self, fun, *args, **kwargs):
