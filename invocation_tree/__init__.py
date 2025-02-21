bc15379bbf2783ec8982009ce6b12c2b5adb3f8b
Bas Terwijn <bterwijn@gmail.com>
2025-02-21 19:49:05 +0100

    update

--- invocation_tree/__init__.py
+++ invocation_tree/__init__.py
@@ -5,7 +5,7 @@
 from graphviz import Digraph
 import html
 import sys
-import inspect
+#import inspect
 import difflib 
 
 __version__ = "0.0.1"
@@ -16,13 +16,13 @@ def highlight_diff(str1, str2):
     result = []
     is_highlighted = False
     for tag, i1, i2, j1, j2 in matcher.get_opcodes():
-        if tag == 'replace':
+        if tag == 'replace' and j1+1<j2:
             result.append(f'<B>{str2[j1:j2]}&#8203;</B>&#8203;')
             is_highlighted = True
-        elif tag == 'delete':
+        elif tag == 'delete' and i1+1<i2:
             result.append(f'<FONT COLOR="#aaaaaa"><I>{str1[i1:i2]}&#8203;</I></FONT>&#8203;')
             is_highlighted = True
-        elif tag == 'insert':
+        elif tag == 'insert' and j1+1<j2:
             result.append(f'<B>{str2[j1:j2]}&#8203;</B>&#8203;')
             is_highlighted = True
         elif tag == 'equal':
@@ -45,7 +45,8 @@ class Tree_Node:
 class Invocation_Tree:
 
     def __init__(self, output_filename='invocation_tree.pdf', show=True, block=True, src_location=True, max_string_len=100, indent='   ',
-                 color_paused = '#ccffcc', color_active = '#ffffff', color_returned = '#ffcccc', each_line=False, to_string=None):
+                 color_paused = '#ccffcc', color_active = '#ffffff', color_returned = '#ffcccc', each_line=False, 
+                 to_string=None, hidden=None):
         # --- config
         self.output_filename = output_filename
         self.show = show
@@ -61,6 +62,9 @@ class Invocation_Tree:
         self.to_string = {}
         if not to_string is None:
             self.to_string = to_string
+        self.hidden = set()
+        if not hidden is None:
+            self.hidden = hidden
         # --- core
         self.stack = []
         self.node_id = 0
@@ -74,41 +78,42 @@ class Invocation_Tree:
         for tree_node in self.stack:
             print(tree_node)
 
-    def value_to_string(self, key, value):
+    def value_to_string(self, key, value, use_repr=False):
         if id(value) in self.to_string:
-            return self.to_string[id(value)](value)
+            val_str = self.to_string[id(value)](value)
         elif key in self.to_string:
-            return self.to_string[key](value)
+            val_str = self.to_string[key](value)
         elif type(value) in self.to_string:
-            return self.to_string[type(value)](value)
+            val_str = self.to_string[type(value)](value)
         else:
-            val_str = str(value)
-            if len(val_str) > self.max_string_len:
-                val_str = '...'+val_str[-self.max_string_len:]    
-            return html.escape(val_str)
+            val_str = repr(value) if use_repr else str(value)
+        if len(val_str) > self.max_string_len:
+            val_str = '...'+val_str[-self.max_string_len:]
+        return html.escape(val_str)
 
-    def get_hightlighted_content(self, tree_node, key, value, old_content=False):
-        if old_content:
+    def get_hightlighted_content(self, tree_node, key, value, use_old_content=False, use_repr=False):
+        if use_old_content:
             return tree_node.strings[key]
         is_highlighted = False
-        content = self.value_to_string(key, value)
+        content = self.value_to_string(key, value, use_repr=use_repr)
         if key in tree_node.strings:
-            old_content = tree_node.strings[key]
-            #print('old:',old_content,'\nnew:',content)
-            hightlighted_content, is_highlighted = highlight_diff(old_content, content)
+            use_old_content = tree_node.strings[key]
+            hightlighted_content, is_highlighted = highlight_diff(use_old_content, content)
         else:
-            hightlighted_content = '<B>'+content+'</B>'
-            is_highlighted = True
+            if len(content.strip())>0: # graphviz error on empty <B></B> tag
+                hightlighted_content = '<B>'+content+'</B>' 
+                is_highlighted = True
+            else:
+                hightlighted_content = content
         tree_node.strings[key] = content
         self.is_highlighted |= is_highlighted
         return hightlighted_content
     
-    def build_html_table(self, tree_node, active=False, is_returned=None, old_content=False):
+    def build_html_table(self, tree_node, active=False, is_returned=None, use_old_content=False):
         if is_returned is None:
             is_returned = tree_node.is_returned
         else:
             tree_node.is_returned = is_returned
-        hightlighted = False
         function_name = tree_node.frame.f_code.co_name
         local_vars = tree_node.frame.f_locals
         return_value = tree_node.return_value
@@ -120,24 +125,28 @@ class Invocation_Tree:
         if is_returned:
             color = self.color_returned
         table = f'<\n<TABLE BORDER="{str(border)}" CELLBORDER="0" CELLSPACING="1" BGCOLOR="{color}">\n  <TR>'
-        hightlighted_content = self.get_hightlighted_content(tree_node, '__function_name__', function_name, old_content)
+        hightlighted_content = self.get_hightlighted_content(tree_node, '__function_name__', function_name, use_old_content)
         table += '<TD ALIGN="left">'+ hightlighted_content +'</TD>'
         for var,val in local_vars.items():
-            if not '__' in var:
+            var_name = function_name+'..'+var
+            val_name = function_name+'.'+var
+            if not '__' in var and not val_name in self.hidden:
                 table += '</TR>\n  <TR>'
-                hightlighted_var = self.get_hightlighted_content(tree_node, function_name+'_'+var, var, old_content)
-                hightlighted_val = self.get_hightlighted_content(tree_node, function_name+'.'+var, val, old_content)
+                hightlighted_var = self.get_hightlighted_content(tree_node, var_name, var, use_old_content)
+                hightlighted_val = self.get_hightlighted_content(tree_node, val_name, val, use_old_content, use_repr=True)
                 hightlighted_content = self.indent + hightlighted_var + ': ' + hightlighted_val
                 table += '<TD ALIGN="left">'+ hightlighted_content  +'</TD>'
         if is_returned:
-            table += '</TR>\n  <TR>'
-            hightlighted_content = self.get_hightlighted_content(tree_node, 'return', return_value, old_content)
-            table += '<TD ALIGN="left">'+ 'return ' + hightlighted_content +'</TD>'
+            return_name = function_name+'.return'
+            if not return_name in self.hidden:
+                table += '</TR>\n  <TR>'
+                hightlighted_content = self.get_hightlighted_content(tree_node, return_name, return_value, use_old_content, use_repr=True)
+                table += '<TD ALIGN="left">'+ 'return ' + hightlighted_content +'</TD>'
         table += '</TR>\n</TABLE>>'
         return table
 
-    def update_node(self, tree_node, active=False, returned=None, old_content=False):
-        table = self.build_html_table(tree_node, active, returned, old_content=old_content)
+    def update_node(self, tree_node, active=False, returned=None, use_old_content=False):
+        table = self.build_html_table(tree_node, active, returned, use_old_content=use_old_content)
         self.node_id_to_table[str(tree_node.node_id)] = table
         
     def update_node_and_log(self, tree_node, active=False, returned=False):
@@ -159,8 +168,7 @@ class Invocation_Tree:
     def output_graph(self, frame, event):
         for idn, node in {(k, v) for k, v in self.update_nodes_log_prev.items() 
                         if k not in self.update_nodes_log}:
-            print('idn:',idn)
-            self.update_node(node, old_content=True) # TODO
+            self.update_node(node, use_old_content=True) # TODO
         graphviz_graph_attr = {}
         graphviz_node_attr = {'shape':'plaintext'}
         graphviz_edge_attr = {}
@@ -173,7 +181,6 @@ class Invocation_Tree:
         for nid1, nid2 in self.edges:
             graph.edge(nid1, nid2)
         if self.block:
-            print('self.is_highlighted', self.is_highlighted)
             if self.is_highlighted:
                 if self.show:
                     graph.view()
