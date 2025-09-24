4a499a74e543d797f54d6b46fdce67cda2cbe398
Bas Terwijn <bterwijn@gmail.com>
2025-09-24 21:29:17 +0200

    config added

---
+++ src/config.py
@@ -0,0 +1,33 @@
+# Some useful invocation_tree configuration examples.
+# Step through this file to see the effects.
+
+def funC():
+    x = 3
+
+def funB():
+    x = 2
+    funC()
+
+def funA():
+    x = 1
+    funB()
+    
+funA()
+
+ivt_tree.hide_vars.add('funA.x')     # hide variable 'x' in funA
+funA()
+
+ivt_tree.hide_vars.add(r're:.*\.x')  # hide all variable 'x' with regex
+funA()
+
+ivt_tree.hide_calls.add('funB')      # hide function call
+funA()
+
+ivt_tree.ignore_calls.add('funB')    # ignore function call (including children)
+funA()
+
+lst = [1, 2, 3]
+# change how type 'list' is shown in the tree:
+ivt_tree.to_string[list] = lambda v : '|'.join(str(i) for i in v)
+# there is bug somewhere that introduces extra white spaces, will look into that
+lst.append(4)
