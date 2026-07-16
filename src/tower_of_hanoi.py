8265cf875524117a41298d6cd6d648f48d44c4d5
Bas Terwijn <bterwijn@gmail.com>
2026-07-16 16:16:55 +0200

    update

--- src/tower_of_hanoi.py
+++ src/tower_of_hanoi.py
@@ -3,10 +3,10 @@ def tower_of_hanoi(n: int):
     
     def move(n, source, target, auxiliary):
         if n == 1:
-            disk = towers[source].pop()
-            towers[target].append(disk)
+            disk = tower[source].pop()
+            tower[target].append(disk)
             print(f'move {disk} from {source} to {target}')
-            print(towers)
+            print(tower)
         else:
             move(n-1, source, auxiliary, target)
             move(1,   source, target, auxiliary)
@@ -14,13 +14,13 @@ def tower_of_hanoi(n: int):
     
     if n <= 0:
         raise ValueError("n must be a positive integer")
-    towers = {
+    tower = {
         "A": list(range(n, 0, -1)),
         "B": [],
         "C": [],
     }    
     print("Initial state:")
-    print(towers)
+    print(tower)
     print()
     move(n, "A", "C", "B")  # start recursion
 
